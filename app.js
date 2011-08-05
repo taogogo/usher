a29162836179d723a9cd71147f6b2d7c1930af88
Howard Rauscher <howardr@gmail.com>
2011-08-05 01:16:07 +0000

    basic files...does nothing really

---
+++ app.js
@@ -0,0 +1,10 @@
+var redis = require('redis');
+
+var client = redis.createClient();
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hi Audrey...there is a train going by right now :(\n');
+}).listen(1337, "0.0.0.0");
+console.log('Server running at http://127.0.0.1:1337/');
