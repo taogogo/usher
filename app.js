caa354e1786262bbb29eed6b367f72bcfe2e93a8
Howard Rauscher <howardr@gmail.com>
2011-08-05 19:06:31 -0500

    testing deploy

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var client = redis.createClient();
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hi Audrey...there is a train going by right now :(\n');
+  res.end('test\n');
 }).listen(1337, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:1337/');
