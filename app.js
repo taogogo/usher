48e3dd25d9d689f621bce3fa203437b153c05408
Howard Rauscher <howardr@gmail.com>
2011-08-08 10:19:03 -0500

    testing redis

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var redis = require('redis');
+var http = require('http');
+
 
 var client = redis.createClient();
 
-var http = require('http');
 http.createServer(function (req, res) {
+  client.incr('hits');
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('test\n');
+  client.get('hits', function(val) {
+    res.end(val);  
+  });
 }).listen(1337, "0.0.0.0");
-console.log('Server running at http://127.0.0.1:1337/');
